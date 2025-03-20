fe5dda30e0d1f89ec36406b75d5b0e289b822b7d
Samarth Rajput <ssamarth224@gmail.com>
2025-03-21 00:49:38 +0530

    created all the json files from the excel sheet

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // Converting Excel file data to json file data
 const XLSX = require("xlsx");
+const fs = require("fs");
+
+// Create an object to store all sheets
+const allSheets = {};
 
 // Creating a constant 
 // readFile() function takes file name as input
 const workbook = XLSX.readFile('./dataset.xlsx');
 // using workbook object we will take Sheet1
-const sheet = workbook.Sheets['Student requests'];
+// Iterating through all sheet names and convert each one to JSON
+workbook.SheetNames.forEach((sheetName) => {
+    const sheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(sheet);
+    allSheets[sheetName] = jsonData;
 
-const jsonData = XLSX.utils.sheet_to_json(sheet);
+    // // Convert json to string
+    // const jsonString = JSON.stringify(jsonData, null, 2);
 
-console.log(jsonData);
+    // // The write file takes 2 argument 
+    // // 1st the file name where we have to write
+    // // 2nd the data that we have to write
+    // const filename = `${sheetName.replace(/ /g, "_").toLowerCase()}.json`
+    // fs.writeFile(filename, jsonString, (err) => {
+    //     console.log(err);
+    // });
+});
+
+const jsonString = JSON.stringify(allSheets, null, 2);
+// write all excel data into a single json file
+fs.writeFile('all_sheets.json', jsonString, (err) => {
+    console.log(err);
+})
